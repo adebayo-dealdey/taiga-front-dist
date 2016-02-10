1560034bbc8c56420b665890b540be6035a98be6
Akinlaja Adebayo <adebayo@dealdey.com>
2016-02-10 15:22:21 +0100

    c1ec58a6ebe7d1cc85c8f2e4194db8f81718ddd7

--- dist/v-1455114086280/js/app-loader.js
+++ dist/v-1455114086280/js/app-loader.js
@@ -1,7 +1,7 @@
 (function() {
   var loadPlugin, loadPlugins, loadStylesheet, promise;
 
-  window._version = "v-1454412984326";
+  window._version = "v-1455114086280";
 
   window.taigaConfig = {
     "api": "http://localhost:8000/api/v1/",
